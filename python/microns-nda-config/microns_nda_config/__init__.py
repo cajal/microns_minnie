618b34450f294b31002d4a3a82d0d5e79fe27fa8
spapa013 <stelios@spapa.us>
2021-11-09 12:30:02 -0600

    updates

--- python/microns-nda-config/microns_nda_config/__init__.py
+++ python/microns-nda-config/microns_nda_config/__init__.py
@@ -1,5 +1,5 @@
 """
-Configuration package/module for microns-coregistration.
+Configuration package/module for microns-nda.
 """
 
 from . import adapters
@@ -15,66 +15,30 @@ except:
 
 from enum import Enum
 
-# Also important to run the dj flags at SOME point during normal datajoint initialization, probably don't want to have this in every config though?
-
-# TODO: place in microns-utils
-def enable_datajoint_flags(enable_python_native_blobs=True):
-    """
-    Enable experimental datajoint features
+from microns_utils import config_utils
     
-    These flags are required by 0.12.0+ (for now).
+config_utils.enable_datajoint_flags()
+
+def register_externals(schema_name:str):
     """
-    dj.config['enable_python_native_blobs'] = enable_python_native_blobs
-    dj.errors._switch_filepath_types(True)
-    dj.errors._switch_adapted_types(True)
-    
-enable_datajoint_flags()
-
-
-def register_externals(schema_name:str=None, stores_config:dict=None):
-    """
-    TODO: Check logic/ add validation if passing both stores_config and schema_name
-
     Registers the external stores for a schema_name in this module.
     """
-    if schema_name is None and stores_config is None:
-        raise Exception("Either schema_name or stores_config must be set")
-
-    if schema_name is not None: 
-        stores_config = externals_mapping[schema_name]
-
-    if 'stores' not in dj.config:
-        dj.config['stores'] = stores_config
-    else:
-        dj.config['stores'].update(stores_config)
+    return config_utils.register_externals(config_mapping[SCHEMAS(schema_name)]["externals"])
 
 
-def register_adapters(schema_name:str=None, adapter_objects:dict=None, context=None):
+def register_adapters(schema_name:str, context=None):
     """
-    TODO: Check logic/ add validation if passing both stores_config and schema_name
-
     Imports the adapters for a schema_name into the global namespace.
-    
-    This function is probably not necessary, but standardization is nice.
     """
-    if schema_name is None and adapter_objects is None:
-        raise Exception("Either schema_name or adapter_objects must be set")
+    return config_utils.register_adapters(config_mapping[SCHEMAS(schema_name)]["adapters"], context=context)
 
-    if schema_name is not None: 
-        adapter_objects = adapters_mapping[schema_name]
-    
-    if context is None:
-        # if context is missing, use the calling namespace
-        import inspect
-        frame = inspect.currentframe().f_back
-        context = frame.f_locals
-        del frame
-    
-    for name, adapter in adapter_objects.items():
-        context[name] = adapter
 
-# IMPORTANT: You can organize several schemas' config files (or folders) however you want as long as
-# the correct schema configurations are plugged in for the enum variations (obviously).
+def create_vm(schema_name:str):
+    """
+    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
+    """
+    schema = config_mapping[SCHEMAS(schema_name)]
+    return config_utils.create_vm(schema.value, external_stores=config_mapping[schema]["externals"], adapter_objects=config_mapping[schema.name]["adapters"])
 
 class SCHEMAS(Enum):
     MINNIE_NDA = 'microns_minnie_nda'
@@ -92,17 +56,3 @@ config_mapping = {
         "adapters": None
     }
 }
-
-adapters_mapping = {SCHEMA.name: config_mapping[SCHEMA]['adapters'] for SCHEMA in SCHEMAS}
-
-externals_mapping = {SCHEMA.name: config_mapping[SCHEMA]['externals'] for SCHEMA in SCHEMAS}
-
-def create_vm(schema_name:str):
-    """
-    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
-    """
-    
-    if externals is not None:
-        register_externals(schema_name)
-    
-    return dj.create_virtual_module(schema_name, schema_name, add_objects=adapters_mapping[schema_name])
--- python/microns-nda/minnie_nda/minnie_nda.py
+++ python/microns-nda/minnie_nda/minnie_nda.py
@@ -11,6 +11,7 @@ config.register_adapters(schema_name, context=locals())
 config.register_externals(schema_name)
 
 schema = dj.schema(schema_name)
+# schema.spawn_missing_classes()
 
 
 @schema
