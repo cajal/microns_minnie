1a0d9c2dbf2549b74dc88fbff893960cd380fad6
spapa013 <stelios@spapa.us>
2021-11-15 16:31:12 -0600

    updates package __init__ for version tracking

--- python/microns-nda/microns_nda/__init__.py
+++ python/microns-nda/microns_nda/__init__.py
@@ -1,10 +1,3 @@
-import warnings
-from microns_utils import config_utils
+from microns_utils import version_utils
 
-_repo = 'microns-nda'
-_package = _repo
-
-try:
-    __version__ = config_utils.get_package_version(repo=_repo, package=_package)
-except:
-    warnings.warn('Package version not able to be determined.')
+__version__ = version_utils.get_package_version(repo='microns-nda', package='microns-nda')
