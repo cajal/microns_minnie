3e8db11a1d12750fc5e64d75e54906f850466a6e
spapa013 <stelios@spapa.us>
2021-11-14 21:36:12 -0600

    updates directory structure and __init__.py

--- python/microns-nda/microns_nda/__init__.py
+++ python/microns-nda/microns_nda/__init__.py
@@ -1,6 +1,6 @@
 from microns_utils import config_utils
 
 _repo = 'microns-nda'
-_package = 'microns-nda-api'
+_package = _repo
 
 __version__ = config_utils.get_package_version(repo=_repo, package=_package)
