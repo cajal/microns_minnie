8728e0d932b22b7fa27fae0a5360bbfd283127ed
spapa013 <stelios@spapa.us>
2021-11-15 12:17:35 -0600

    add try/ catch for version

--- python/microns-nda-api/microns_nda_api/__init__.py
+++ python/microns-nda-api/microns_nda_api/__init__.py
@@ -1,6 +1,10 @@
+import warnings
 from microns_utils import config_utils
 
 _repo = 'microns-nda'
 _package = 'microns-nda-api'
 
-__version__ = config_utils.get_package_version(repo=_repo, package=_package)
+try:
+    __version__ = config_utils.get_package_version(repo=_repo, package=_package)
+except:
+    warnings.warn('Package version not able to be determined.')
