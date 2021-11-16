60743eb6dd37735b20b9aca73621b5aa5ba58228
spapa013 <stelios@spapa.us>
2021-11-16 10:06:59 -0600

    updates version control

--- python/microns-nda/microns_nda/__init__.py
+++ python/microns-nda/microns_nda/__init__.py
@@ -1,3 +1,14 @@
 from microns_utils import version_utils
 
-__version__ = version_utils.get_package_version(repo='microns-nda', package='microns-nda')
+__version__ = version_utils.get_package_version(
+    package='microns-nda', 
+    check_if_latest=True, 
+    check_if_latest_kwargs=dict(
+        repo='microns-nda', 
+        user='cajal', 
+        branch='main', 
+        source='commit', 
+        path_to_version_file='python/version.py', 
+        tag=None
+    )
+)
