97a92d42988121704135a63d34dfa8d9e17d220e
spapa013 <stelios@spapa.us>
2021-11-18 09:10:23 -0600

    adds check_latest_version

--- python/microns-nda/microns_nda/__init__.py
+++ python/microns-nda/microns_nda/__init__.py
@@ -1,6 +1,6 @@
 from microns_utils import version_utils
 
-__version__ = version_utils.get_package_version(
+__version__ = version_utils.check_package_version(
     package='microns-nda', 
     check_if_latest=True, 
     check_if_latest_kwargs=dict(
@@ -9,3 +9,9 @@ __version__ = version_utils.get_package_version(
         source='tag', 
     )
 )
+
+def check_latest_version_from_github(owner='cajal', repo='microns-nda', source='tag', branch=None, path_to_version_file=None, warn=True):
+    """
+    Wrapper for :func:`~microns_utils.version_utils.check_latest_version_from_github`
+    """
+    return version_utils.check_latest_version_from_github(owner=owner, repo=repo, source=source, branch=branch, path_to_version_file=path_to_version_file, warn=warn)
