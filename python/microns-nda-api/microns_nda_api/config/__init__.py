6fd99613a3eecb516b7c932dfc15ba899352bd23
spapa013 <stelios@spapa.us>
2021-11-21 21:10:25 -0600

    adds register_bases compatibility

--- python/microns-nda-api/microns_nda_api/config/__init__.py
+++ python/microns-nda-api/microns_nda_api/config/__init__.py
@@ -1,23 +1,21 @@
 """
 Configuration package/module for microns-nda.
 """
-
+import inspect
+import traceback
+from enum import Enum
 from . import adapters
 from . import externals
-
-import traceback
-
+from . import bases
 try:
     import datajoint as dj
 except:
     traceback.print_exc()
     raise ImportError('DataJoint package not found.')
-
-from enum import Enum
-
 from microns_utils import config_utils
     
 config_utils.enable_datajoint_flags()
+
 
 def register_externals(schema_name:str):
     """
@@ -39,12 +37,28 @@ def register_adapters(schema_name:str, context=None):
         config_utils.register_adapters(adapter_objects, context=context)
 
 
+def register_bases(schema_name:str, module):
+    """
+    Maps base classes to DataJoint tables.
+    """
+    bases = config_mapping[SCHEMAS(schema_name)]["bases"]
+
+    if bases is not None:
+        for base in bases:
+            config_utils.register_bases(base, module)
+        return module
+
+
 def create_vm(schema_name:str):
     """
-    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
+    Creates a virtual module after registering the external stores, adapter objects, DatajointPlus and base classes.
     """
     schema = SCHEMAS(schema_name)
-    return config_utils.create_vm(schema.value, external_stores=config_mapping[schema]["externals"], adapter_objects=config_mapping[schema]["adapters"])
+    vm = config_utils._create_vm(schema.value, external_stores=config_mapping[schema]["externals"], adapter_objects=config_mapping[schema]["adapters"])
+    config_utils.add_datajoint_plus(vm)
+    register_bases(schema_name, vm)
+    return vm
+
 
 class SCHEMAS(Enum):
     MINNIE_NDA = 'microns_minnie_nda'
@@ -53,12 +67,14 @@ class SCHEMAS(Enum):
 
 config_mapping = {
     SCHEMAS.MINNIE_NDA: {
-        "externals": None,
-        "adapters": None
+        "externals" : None,
+        "adapters" : None,
+        "bases" : None
     },
 
     SCHEMAS.MINNIE_FUNCTION: {
         "externals": None,
-        "adapters": None
+        "adapters": None,
+        "bases" : None
     }
 }
