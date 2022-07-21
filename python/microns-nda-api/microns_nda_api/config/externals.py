f681ad4fc1a37073f23c238c836f93743aa8b220
Zhuokun Ding <zkding@outlook.com>
2022-07-20 23:59:25 -0500

    implement external for correlation matrix

--- python/microns-nda-api/microns_nda_api/config/externals.py
+++ python/microns-nda-api/microns_nda_api/config/externals.py
@@ -1,5 +1,15 @@
+
 """
 Externals for DataJoint tables.
 """
+
+from pathlib import Path
+import datajoint_plus as djp
+
+base_path = Path() / '/mnt' / 'dj-stor01' / 'microns' / 'minnie65' / 'nda'
 minnie_nda = {}
-minnie_function = {}
+
+function_path = base_path / 'function'
+minnie_function = {
+    'resp_array': djp.make_store_dict(function_path / 'resp_array'),
+}
