7d903b05690faeaa974275699181d7523d6b5bbb
Zhuokun Ding <zkding@outlook.com>
2022-10-04 02:13:44 -0500

    deploy

---
+++ deploy/zhuokund/script/noise_corr.py
@@ -0,0 +1,4 @@
+from microns_nda_api.schemas import minnie_function
+
+minnie_function.NoiseCorr.populate(reserve_jobs=True, order='random')
+minnie_function.BehMod.populate(reserve_jobs=True, order='random')
